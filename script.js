dbefd454931ddbc5591bbf6c4dde454ae526ffe4
25jdavids <25jdavids@wakatipu.school.nz>
2025-05-26 10:34:21 +1200

    Added home screen and main page

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function greet() {
-    alert("Welcome to my website!");
+    alert("Welcome to the Queenstown bus management website where you can track, manage and enjoy the public bus system at your full and best disposal!");
+}
+function begin() {
+    window.location.href = 'Main.html';
 }
