8468aa56399bae683a88ff653986600b02a3d036
25jdavids <25jdavids@wakatipu.school.nz>
2025-06-03 09:43:26 +1200

    Added a main page and a few links to all of my other pages

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function greet() {
-    alert("Welcome to the Queenstown bus management website where you can track, manage and enjoy the public bus system at your full and best disposal!");
-}
 function begin() {
     window.location.href = 'Main.html';
 }
+
+
